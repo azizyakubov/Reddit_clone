578ff3153adf7783f91c150fdbf1d71117b20a2a
Aziz Yakubov <azizy15@gmail.com>
2019-02-14 15:37:32 -0800

    modified links to show friendly slugs

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,4 +5,11 @@ class Channel < ApplicationRecord
   # Check the schema for reference
   has_many :users, through: :discussions
   resourcify
+
+  extend FriendlyId
+  friendly_id :channel, use: [:slugged, :finders]
+
+  def should_generate_new_friendly_id?
+    channel_changed?
+  end
 end
